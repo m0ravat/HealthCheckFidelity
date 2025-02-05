594bfc1d41aa142a1e3f38fd44955116709cbce0
Muhammad Ravat <m0ravat736@gmail.com>
2025-02-05 18:03:21 +0000

    created signup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // This scans all HTML files in the root folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
